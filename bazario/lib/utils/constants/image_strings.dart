db635464b2a14771da1163865b8fcb6de7521153
Hassnaa9 <hm2922@fayoum.edu.eg>
2025-08-17 10:28:24 +0300

    refactor: add new images urls

--- bazario/lib/utils/constants/image_strings.dart
+++ bazario/lib/utils/constants/image_strings.dart
@@ -1,11 +1,25 @@
 class ImagesUrl {
-  static const String splashScreen ="assets/images/splash_screen.png";
+  static const String splashScreen ="assets/images/splash_screen.jpeg";
   static const String welcome1 = "assets/images/welcome1.png";
   static const String welcome2 = "assets/images/welcome2.png";
   static const String welcome3 = "assets/images/welcome3.png";
   static const String leftArrow = "assets/icons/left_arrow.png";
   static const String rightArrow = "assets/icons/right-arrow.png";
   static const String enterArrow = "assets/icons/enter.png";
-
-
+  static const String appleIcon = "assets/icons/apple.png";
+  static const String facebookIcon = "assets/icons/Facebook.png";
+  static const String googleIcon = "assets/icons/Google.png";
+  static const String bannerImg = "assets/images/banner.png";
+  static const String prod1Img = "assets/images/prod1.png";
+  static const String prod2Img = "assets/images/prod2.jpg";
+  static const String dressIcon = "assets/icons/dress.png";
+  static const String jacketIcon = "assets/icons/jacket.png";
+  static const String pantsIcon = "assets/icons/jeans.png";
+  static const String tshirtIcon = "assets/icons/tshirt.png";
+  static const String prod3Img = "assets/images/prod3.png";
+  static const String prod4Img = "assets/images/prod4.webp";
+  static const String prod5Img = "assets/images/prod5.jpeg";
+  static const String paypal = "assets/images/paypal_logo.jpeg";
+  static const String applePay = "assets/images/apple_pay.jpeg";
+  static const String googlePay = "assets/images/google_pay.jpeg";
 }
