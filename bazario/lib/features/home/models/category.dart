fa6bdf50520be814c3fa8b79a65301936aa733b4
Hassnaa9 <hm2922@fayoum.edu.eg>
2025-08-17 10:25:17 +0300

    feat: add category model

---
+++ bazario/lib/features/home/models/category.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  final String name;
+  final String icon; // Using IconData for simplicity, but you can use image paths
+
+  Category({required this.name, required this.icon});
+}
