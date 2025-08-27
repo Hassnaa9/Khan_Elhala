161f175b5bf6b1a06037c6bd28109b8fa656f058
Hassnaa9 <hm2922@fayoum.edu.eg>
2025-08-27 19:11:48 +0300

    fix: Display items in order list

--- bazario/lib/user_features/checkout&payment/screens/widgets/order_list.dart
+++ bazario/lib/user_features/checkout&payment/screens/widgets/order_list.dart
@@ -1,58 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
 
 import '../../../../data/repositories/home_provider.dart';
 import 'package:provider/provider.dart'; // Import Provider
+import '../../../home/models/cart_item.dart';
 
 class OrderList extends StatelessWidget {
   const OrderList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final cartItems = Provider.of<HomeProvider>(context).cartItems;
-    return ListView.builder(
-        physics: const NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
-        itemCount: cartItems.length,
-        itemBuilder: (context, index) {
-          final item = cartItems[index];
-          return Card(
-            margin: const EdgeInsets.symmetric(vertical: 8),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15)),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.network(
-                      item.product.imageUrl,
-                      width: 80,
-                      height: 80,
-                      fit: BoxFit.cover,
+    return ValueListenableBuilder<Box<CartItem>>(
+      valueListenable: Hive.box<CartItem>('cartBox').listenable(),
+      builder: (BuildContext context, box, Widget? child) {
+        final cartItems = box.values.toList();
+        final isCartNotEmpty = cartItems.isNotEmpty;
+        return ListView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: cartItems.length,
+          itemBuilder: (context, index) {
+            final item = cartItems[index];
+            return Card(
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        item.product.imageUrl,
+                        width: 80,
+                        height: 80,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 10),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(item.product.name,
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold)),
-                      const SizedBox(height: 5),
-                      Text('Size: XL',
-                          style: TextStyle(color: Colors.grey[600])),
-                      const SizedBox(height: 5),
-                      Text('\$${item.product.price.toStringAsFixed(2)}',
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold)),
-                    ],
-                  ),
-                ],
+                    const SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(item.product.name,
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                        const SizedBox(height: 5),
+                        Text('Size: XL',
+                            style: TextStyle(color: Colors.grey[600])),
+                        const SizedBox(height: 5),
+                        Text('\$${item.product.price.toStringAsFixed(2)}',
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          );
-        },
-      );
-    }}
-
+            );
+          },
+        );
+      },
+    );
+  }
+}
